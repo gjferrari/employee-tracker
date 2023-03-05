b8e04b7860d77d110fe99652a897477ba0bc5649
Genevieve Ferrari <genferrari@gmail.com>
2023-03-04 20:09:12 -0800

    edited prompt

--- connection.js
+++ connection.js
@@ -1,5 +1,7 @@
 const dotenv = require("dotenv");
 const cTable = require("console.table");
+const inquirer = require('inquirer');
+const { left } = require("inquirer/lib/utils/readline");
 
 const db = mysql.createConnection(
   {
@@ -11,8 +13,65 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee database.`)
 );
 
-// db.query("SELECT * FROM students", function (err, results) {
-//   console.log(results);
-// });
+async fuction loadMainPromt () {
+  return inquirer
+  .prompt([
+    {
+    type: "list",
+    name: "choice",
+    message: "What do you want to do?",
+    choices: [{
+        name: "View all departments",
+        value: "VIEW_DEPARTMENTS",
+      },
+      {
+        name: "View all roles",
+        value:"VIEW_ROLES",
 
-//I really do not understand this
+      },
+      {
+        name: "View all employees",
+        valuee:"VIEW_EMPLOYEES",
+
+      },
+      {
+        name: "Add a departments",
+        value: "ADD_DEPARTMENT",
+      },
+      {
+        name: "Add a role",
+        valuee:"ADD_ROLE",
+
+      },
+      {
+        name: "Add an employees",
+        valuee:"ADD_EMPLOYEE",
+
+      },
+      {
+        name: "Update employee",
+        valuee:"UPDATE_EMPLOYEE",
+
+      },
+    ]} ])
+  
+
+//     {
+//       type: "input",
+//       name: "username",
+//       message: "What is your github username?",
+//     },
+
+//     {
+//       type: "input",
+//       name: "email",
+//       message: "What is your email address?",
+//     },
+// }) =>
+//   `#`
+
+
+
+
+// db.query("SELECT * FROM department", function (err, results) {
+//   console.log(results);;

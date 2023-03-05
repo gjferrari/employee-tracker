bda1db62e85a194d5bf017e99450cec4ceeed77d
Genevieve Ferrari <genferrari@gmail.com>
2023-03-04 21:45:05 -0800

    added basic function to begin writing queries

--- index.js
+++ index.js
@@ -4,41 +4,79 @@ const inquirer = require("inquirer");
 const { left } = require("inquirer/lib/utils/readline");
 
 async function loadMainPromt() {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "What do you want to do?",
-      choices: [
+  return (
+    inquirer
+      .prompt([
         {
-          name: "View all departments",
-          value: "VIEW_DEPARTMENTS",
+          type: "list",
+          name: "choice",
+          message: "What do you want to do?",
+          choices: [
+            {
+              name: "View all departments",
+              value: "VIEW_DEPARTMENTS",
+            },
+            {
+              name: "View all roles",
+              value: "VIEW_ROLES",
+            },
+            {
+              name: "View all employees",
+              value: "VIEW_EMPLOYEES",
+            },
+            {
+              name: "Add a department",
+              value: "ADD_DEPARTMENT",
+            },
+            {
+              name: "Add a role",
+              value: "ADD_ROLE",
+            },
+            {
+              name: "Add an employee",
+              value: "ADD_EMPLOYEE",
+            },
+            {
+              name: "Update employee",
+              value: "UPDATE_EMPLOYEE",
+            },
+          ],
         },
-        {
-          name: "View all roles",
-          value: "VIEW_ROLES",
-        },
-        {
-          name: "View all employees",
-          value: "VIEW_EMPLOYEES",
-        },
-        {
-          name: "Add a departments",
-          value: "ADD_DEPARTMENT",
-        },
-        {
-          name: "Add a role",
-          value: "ADD_ROLE",
-        },
-        {
-          name: "Add an employees",
-          value: "ADD_EMPLOYEE",
-        },
-        {
-          name: "Update employee",
-          value: "UPDATE_EMPLOYEE",
-        },
-      ],
-    },
-  ]);
+      ])
+
+      // https://www.w3schools.com/js/js_switch.asp
+      .then((answers) => {
+        switch (answers.choice) {
+          case "View all deparments":
+            viewDepartments();
+            break;
+          case "View all roles":
+            viewRoles();
+            break;
+          case "View all employees":
+            viewEmployees();
+            break;
+          case "Add a department":
+            addDeparment();
+            break;
+          case "Add a role":
+            addRole();
+            break;
+          case "Add an employee":
+            addEmployee();
+            break;
+          case "Update employee":
+            updateEmployee();
+            break;
+        }
+      })
+  );
 }
+
+function viewDepartments() {}
+function viewRoles() {}
+function viewEmployees() {}
+function addDeparment() {}
+function addRole() {}
+function addEmployee() {}
+function updateEmployee() {}
